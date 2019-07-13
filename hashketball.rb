9382a2ded292ddd7db61f0e64d68ad233f858679
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 18:07:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_stats(name)
       if c == :players
         d.each do |e,f|
           if e[:player_name] == name
-            return e[:shoe]
+            return e.delete(:player_name)
           end
         end
       end
